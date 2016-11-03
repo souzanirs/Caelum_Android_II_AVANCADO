c4ffffae43914f7428bab298f3e4729e09a2b41f
souzanirs@gmail.com <souzanirs@gmail.com>
2016-11-03 12:30:35 -0200

    Colocando uma notificação na barra de notificações

--- CarangosApp/src/main/java/br/com/caelum/fj59/carangos/gcm/GCMBroadcastReceiver.java
+++ CarangosApp/src/main/java/br/com/caelum/fj59/carangos/gcm/GCMBroadcastReceiver.java
@@ -1,9 +1,15 @@
 package br.com.caelum.fj59.carangos.gcm;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
+import android.os.Build;
+import br.com.caelum.fj59.carangos.R;
+import br.com.caelum.fj59.carangos.activity.MainActivity;
 import br.com.caelum.fj59.carangos.infra.MyLog;
 
 /**
@@ -11,8 +17,31 @@ import br.com.caelum.fj59.carangos.infra.MyLog;
  */
 
 public class GCMBroadcastReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
         MyLog.i("CHEGOU A MENSAGEM DO GCM!");
+
+        String mensagem = (String) intent.getExtras().getSerializable("message");
+
+        MyLog.i("----->>> MENSAGEM COM CONTEUDO: "+mensagem);
+
+        Intent irParaLeilao = new Intent(context, MainActivity.class);
+
+        PendingIntent acaoPendente = PendingIntent.getActivity(context, 123, irParaLeilao, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        irParaLeilao.putExtra("idDaNotificacao",Constantes.ID_NOTIFICACAO);
+
+        Notification notificacao = new Notification.Builder(context)
+                .setContentTitle("Um novo leilão começou!")
+                .setContentText("Leilão: "+mensagem)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(acaoPendente)
+                .build();
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+
+        manager.notify(Constantes.ID_NOTIFICACAO, notificacao);
+
     }
 }
